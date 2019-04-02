ca87129f46481f17adf76f746a7ab2fa4ed0d9f6
Michael Yankura <myankura210@gmail.com>
2019-04-02 12:38:09 -0500

    completed doctors exercise.

--- doctor.js
+++ doctor.js
@@ -1,1 +1,25 @@
 console.log("doctor.js");
+
+//part one of Doctor exercise.  (See README)
+const createDoctor = (name, speciality, address) => ({
+    "name": name,
+    "speciality": speciality,
+    "address": address
+});
+
+//part two of Doctor exercise. (See README)
+const createPet = (name, breed) => ({
+    "name": name,
+    "breed": breed
+});
+
+const bowWowKennels = [];
+
+//Invoke createPet three times
+const pet1 = createPet("Race", "Labrador");
+const pet2 = createPet("Roxy", "Beagador");
+const pet3 = createPet("Riley", "Texas Healer");
+
+//Push each new pet object into an array called bowWowKennels (see README)
+bowWowKennels.push(pet1, pet2, pet3);
+console.log(bowWowKennels);
